7fbe1223e9289644bc113a7d0954bf0cc319c3ea
gan <gan58bc58@163.com>
2019-02-19 18:49:47 +0800

    主页

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Home">
+        {{msg}}
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'Home',
+    data (){
+        return{
+            msg:'主页'
+        }
+    },
+    components:{
+
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
